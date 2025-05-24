36ccb04850d857cffe186482ef91c4fec2eed86d
bcarvalho99 <bcarvalho5699@gmail.com>
2025-05-24 15:32:18 -0300

    adicionaprimeira validação de esquema em teste de contrato

--- testes/contrato/getAllProducts.contract.test.js
+++ testes/contrato/getAllProducts.contract.test.js
@@ -1,14 +1,37 @@
 const {spec} = require('pactum');
-
+const Joi = require('joi');
 
 const apiURL = 'https://automationexercise.com/api';
+
+const schema = Joi.object({
+id: Joi.number().required(),
+name:Joi.string().required(),
+price: Joi.string().required(),
+brand: Joi.string().required(),
+category:Joi.object({
+    usertype: Joi.object({
+        usertype: Joi.string().required()
+    }).required(),
+    category: Joi.string().required()
+}).required()
+});
+
+const respSchema = Joi.object({
+    responseCode: Joi.number().valid(200).required(),
+    products: Joi.array().items(schema).required(),
+
+});
 
 describe('Produtos - Testes de Contrato', ()=>{
 
 test('Validar resposta com código de status 200', async()=>{
-    await spec().get(`${apiURL}/productsList`)
-    .expectStatus(200);
+    const resp = await spec().get(`${apiURL}/productsList`)
+    .expectStatus(200) 
+    .returns('res.body'); 
     
-})
+    
+    const { error } = respSchema.validate(resp);
+    expect(error).toBeUndefined()
+});
 
 })
