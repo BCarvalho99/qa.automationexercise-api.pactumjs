fe8aace42b84b6cd6b4b8a7dfc5207563c9b7451
bcarvalho99 <bcarvalho5699@gmail.com>
2025-05-25 01:49:15 -0300

    refatoracao de codigo

--- tests/schemas/productSchema.js
+++ tests/schemas/productSchema.js
@@ -1,5 +1,4 @@
-const Joi = require('joi');
-
+const Joi = require("joi");
 
 const productSchema = Joi.object({
   id: Joi.number().required(),
@@ -8,19 +7,18 @@ const productSchema = Joi.object({
   brand: Joi.string().required(),
   category: Joi.object({
     usertype: Joi.object({
-      usertype: Joi.string().required()
+      usertype: Joi.string().required(),
     }).required(),
-    category: Joi.string().required()
-  }).required()
+    category: Joi.string().required(),
+  }).required(),
 });
-
 
 const respSchema = Joi.object({
   responseCode: Joi.number().valid(200).required(),
-  products: Joi.array().items(productSchema).required()
+  products: Joi.array().items(productSchema).required(),
 });
 
 module.exports = {
   productSchema,
-  respSchema
+  respSchema,
 };
